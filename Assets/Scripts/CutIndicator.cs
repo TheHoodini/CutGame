8f24472ed5419ecc4bac9ad2369fbed6043ce505
Juan Diego García <salasjuan@uninorte.edu.co>
2026-05-17 03:15:45 -0700

    feat: add cut indicator
    Cut Indicator and cut based on angle (with hold)

--- Assets/Scripts/CutIndicator.cs
+++ Assets/Scripts/CutIndicator.cs
@@ -1,16 +1,24 @@
+using System.Collections;
 using UnityEngine;
 
 public class CutIndicator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Rotate(float timeSeconds = 0.1f, float angle = -45)
     {
-        
+        StartCoroutine(RotateCoroutine(angle, timeSeconds));
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator RotateCoroutine(float angle, float timeSeconds)
     {
-        
+        float elapsedTime = 0f;
+        Quaternion initialRotation = transform.rotation;
+        Quaternion targetRotation = initialRotation * Quaternion.Euler(0, 0, angle);
+        while (elapsedTime < timeSeconds)
+        {
+            transform.rotation = Quaternion.Slerp(initialRotation, targetRotation, elapsedTime / timeSeconds);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.rotation = targetRotation;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Interactions;
@@ -5,27 +6,80 @@ using UnityEngine.InputSystem.Interactions;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private CuttableObject _cuttableObject;
+    [SerializeField] private float _rotationInterval = 0.2f;
+    
+    private float _holdActivationDelay = 0.25f; 
     private CharacterController _characterController;
+    private CutIndicator _cutIndicator;
+    private bool _isHolding = false;
+    private bool _isTap = false;
+    private float _rotationTimer = 0f;
+    private float _holdDelayTimer = 0f;
+    private bool _holdConfirmed = false;
+
     private void Awake()
     {
+        _cutIndicator = FindFirstObjectByType<CutIndicator>();
         _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (!_isHolding) return;
+
+        if (!_holdConfirmed)
+        {
+            _holdDelayTimer += Time.deltaTime;
+            if (_holdDelayTimer >= _holdActivationDelay)
+            {
+                _holdConfirmed = true;
+                _rotationTimer = _rotationInterval; 
+            }
+            return;
+        }
+
+        _rotationTimer += Time.deltaTime;
+        if (_rotationTimer >= _rotationInterval)
+        {
+            _cutIndicator.Rotate();
+            _rotationTimer = 0f;
+        }
     }
 
     public void OnAction(InputAction.CallbackContext context)
     {
-        if (context.interaction is TapInteraction &&
-            context.performed)
+        // TAP
+        if (context.interaction is TapInteraction)
         {
-            Debug.Log("Tap");
-                _cuttableObject.CutAtAngle(_cuttableObject.cutAngle);
+            if (context.started)
+                _isTap = true;
+
+            if (context.performed && _isTap)
+            {
+                Debug.Log("Tap");
+                _cuttableObject.CutAtAngle(Math.Abs(_cutIndicator.transform.rotation.eulerAngles.z) * -1);
+                _isTap = false;
+            }
         }
 
         // HOLD
-        if (context.interaction is HoldInteraction &&
-            context.performed)
+        if (context.interaction is HoldInteraction && !_isTap)
         {
-            Debug.Log("Hold");
+            if (context.started)
+            {
+                Debug.Log("Holding");
+                _isHolding = true;
+                _holdConfirmed = false;
+                
+            }
+            else if (context.canceled)
+            {
+                Debug.Log("Hold Ended");
+                _isHolding = false;
+                _holdConfirmed = false;
+            }
+            _holdDelayTimer = 0f;
+            _rotationTimer = 0f;
         }
     }
-    
 }
